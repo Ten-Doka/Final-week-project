78a1db04224457c00373c63622a6a0758f014386
Ten-Doka <tdolker51@gmail.com>
2024-01-13 23:19:06 -0500

    Added weather forecast

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ function currentTemp(response) {
 
   let condition = document.querySelector(".condition");
   condition.innerHTML = response.data.condition.description;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -45,16 +47,57 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "a15064fo064a4647d4a8bf3bt0bb11df";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(currentTemp);
 }
+
 function searchCityElement(event) {
   event.preventDefault();
   let cityElement = document.querySelector("#search-input-city");
 
   searchCity(cityElement.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="forecast-day">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="forecast-icon" />
+        <div class="forecast-temperatures">
+          <div class="maximum">
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
+          </div>  
+          <div class="minimum">${Math.round(day.temperature.minimum)}°</div>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchCityButton = document.querySelector("#search-input");
 searchCityButton.addEventListener("submit", searchCityElement);
 
